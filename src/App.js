d07c96a2a9be2ca96cf76617c1fc55a45b59c2a4
Geoffrey Schuchardt <gschuchardt@budget-mentor.com>
2022-04-24 15:09:05 -0400

    added eta

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <h1>Budget Mentor</h1>
         <p>Coming soon...</p>
+        <p>(ETA: 8/1/2022)</p>
       </header>
     </div>
   );
